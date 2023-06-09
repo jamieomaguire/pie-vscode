6736f18806d426ec5197c52ae15654999fa0a27a
Jamie Maguire <jamie.maguire@just-eat.com>
2023-06-09 13:17:40 +0100

    tidy code

--- src/extension.ts
+++ src/extension.ts
@@ -1,65 +1,87 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	let workspaceFolders = vscode.workspace.workspaceFolders;
+interface CssVariableCompletion {
+    prefix: string;
+    body: string;
+    description: string;
+}
+
+/**
+ * Gets a object containing the completion data for each design token CSS variable
+ * @param cssVariableFileString the string contents of the CSS variable file to parse
+ * @returns 
+ */
+function getCssVariablesCompletions(cssVariableFileString: string): Record<string, CssVariableCompletion> {
+	const cssVariables: string[] = cssVariableFileString.match(/--dt-.*?:/g)?.map(variable => variable.replace(':', '')) || [];
+	
+	return cssVariables.reduce((accumulatedValues: Record<string, CssVariableCompletion>, variable: string) => {
+		const prefix = `${variable.replace('--', '')}`;
+		
+		accumulatedValues[variable] = {
+			prefix,
+			body: `var(${variable})`,
+			description: `Some neat description for \`${variable}\` goes here! \n\n [pie.design reference](https://pie.design/foundations/colour/tokens/global#${prefix})`,
+		};
+
+		return accumulatedValues;
+	}, {}) || {};
+}
+
+/**
+ * Creates a vscode.CompletionList from the provided cssVariablesObject
+ * @param cssVariablesObject 
+ * @returns 
+ */
+function createCompletionItems(cssVariablesObject: Record<string, CssVariableCompletion>): vscode.CompletionList {
+	const completionItems = new vscode.CompletionList();
+
+	for (const key in cssVariablesObject) {
+		const completionItem = new vscode.CompletionItem(cssVariablesObject[key].prefix);
+		completionItem.insertText = cssVariablesObject[key].body;
+		const docs = new vscode.MarkdownString();
+		docs.appendMarkdown(cssVariablesObject[key].description);
+		completionItem.documentation = docs;
+		completionItem.label = `${cssVariablesObject[key].prefix.replace('dt-', '')} - PIE Design Token`;
+		completionItem.kind = cssVariablesObject[key].prefix.includes('color') ? vscode.CompletionItemKind.Color : vscode.CompletionItemKind.Variable;
+		completionItems.items.push(completionItem);
+	}
+
+	return completionItems;
+}
+
+export function activate(context: vscode.ExtensionContext): void {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
 
 	if (!workspaceFolders) {
 		vscode.window.showInformationMessage('Could not locate pie design tokens in node_modules :(');
-	} else {
-		vscode.window.showInformationMessage('Located pie design tokens in node_modules :)');
-
-		const currentWorkspacePath = workspaceFolders[0].uri.fsPath;
-		const nodeModulesFilePath = path.join(currentWorkspacePath, 'node_modules', '@justeat', 'pie-design-tokens', 'dist', 'jet.css');
-
-		// retrieve the design tokens css file from the current workspace's node_modules folder
-		vscode.workspace.openTextDocument(nodeModulesFilePath).then((document) => {
-			const data = document.getText();
-			const cssVariables = data.match(/--dt-.*?:/g)?.map((variable: string) => variable.replace(':', ''));
-			// todo: this is old code and can be refactored and moved to the provider below
-			const cssVariablesObject = cssVariables?.reduce((acc: { [key: string]: any }, variable: string) => {
-				acc[variable] = {
-					prefix: `${variable.replace('--', '')}`,
-					body: `var(${variable})`,
-					description: `Some neat description for \`${variable}\` goes here! \n\n [pie.design reference](https://pie.design/foundations/colour/tokens/global/#${variable.replace('--', '')})`,
-				};
-				return acc;
-			}, {}) || {};
-
-			const provider = {
-				provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-					const completionItems = new vscode.CompletionList();
-					// todo: can we split global and alias tokens and adjust label accordingly?
-					Object.keys(cssVariablesObject).forEach((key) => {
-						const completionItem = new vscode.CompletionItem(cssVariablesObject?.[key].prefix);
-						completionItem.insertText = cssVariablesObject?.[key].body;
-						const docs = new vscode.MarkdownString();
-						docs.appendMarkdown(cssVariablesObject?.[key].description);
-						completionItem.documentation = docs;
-						completionItem.label = `${cssVariablesObject?.[key].prefix.replace('dt-', '')} - PIE Design Token`;
-						completionItem.kind = cssVariablesObject?.[key].prefix.includes('color') ? vscode.CompletionItemKind.Color : vscode.CompletionItemKind.Variable;
-
-						completionItems.items.push(completionItem);
-					});
-					return completionItems;
-				}
-			};
-			
-			const disposable = vscode.languages.registerCompletionItemProvider(['css', 'scss'], provider);
-
-			if (disposable) {
-				context.subscriptions.push(disposable);
-				vscode.window.showInformationMessage('Created pie design token snippets :D');
-			} else {
-				vscode.window.showInformationMessage('Failed to create pie design token snippets :(');
-			}
-		});
+		
+		return;
 	}
+
+	const currentWorkspacePath = workspaceFolders[0].uri.fsPath;
+	const nodeModulesFilePath = path.join(currentWorkspacePath, 'node_modules', '@justeat', 'pie-design-tokens', 'dist', 'jet.css');
+
+	vscode.workspace.openTextDocument(nodeModulesFilePath).then((document: vscode.TextDocument) => {
+		vscode.window.showInformationMessage('Located pie design tokens in node_modules :)');
+		const data = document.getText();
+		const cssVariableCompletions = getCssVariablesCompletions(data);
+
+		const provider: vscode.CompletionItemProvider = {
+			provideCompletionItems(): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
+				return createCompletionItems(cssVariableCompletions);
+			}
+		};
+
+		const disposable = vscode.languages.registerCompletionItemProvider(['css', 'scss'], provider);
+
+		if (disposable) {
+			context.subscriptions.push(disposable);
+			vscode.window.showInformationMessage('Created pie design token snippets :D');
+		} else {
+			vscode.window.showInformationMessage('Failed to create pie design token snippets :(');
+		}
+	});
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
