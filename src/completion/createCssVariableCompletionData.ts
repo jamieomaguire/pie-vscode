ffda831faf2cb479b4d51b63cc0bf84904e49e9d
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-07-19 14:06:03 +0100

    Add isGlobal

--- src/completion/createCssVariableCompletionData.ts
+++ src/completion/createCssVariableCompletionData.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
 import { CssVariableCompletionGroup, CssVariableTokenCompletions, TokenTypes } from '../types';
 
-const getTokenDetail = (tokenValue: string, isColor: boolean) : string | undefined => {
-    if (isColor) {
+const getColorTokenDetail = (tokenValue: string, isGlobal : boolean) : string | undefined => {
+    if (isGlobal) {
         return tokenValue.replace(/;/, '').trim();
     }
     return undefined;
@@ -14,6 +14,7 @@ export function createCssVariableCompletionData(cssString: string): CssVariableT
     const globalTokenMessage = '**Warning** - Try to use Alias tokens instead of Globals.';
 
     let currentGroup: { [tokenType: string]: CssVariableCompletionGroup } | null = null;
+    let isGlobal = false;
     const lines = cssString.split('\n');
 
     for (let line of lines) {
@@ -21,13 +22,16 @@ export function createCssVariableCompletionData(cssString: string): CssVariableT
 
         if (line.startsWith('/* Global tokens')) {
             currentGroup = global;
+            isGlobal = true;
+
         } else if (line.startsWith('/* Alias tokens')) {
             currentGroup = alias;
+            isGlobal = false;
         } else if (line.startsWith('--')) {
             const [variableName, variableValue] = line.split(':');
 
             if (currentGroup) {
-				const prefix = `${variableName.replace('--', '')}`;
+                const prefix = `${variableName.replace('--', '')}`;
                 const tokenType = prefix.split('-')[1] as TokenTypes;
 
                 if (!currentGroup[tokenType]) {
@@ -38,10 +42,10 @@ export function createCssVariableCompletionData(cssString: string): CssVariableT
 
                 currentGroup[tokenType][variableName] = {
 					body: `var(${variableName})`,
-					description: `Some neat description for \`${variableName}\` goes here! \n\n ${currentGroup === global ? globalTokenMessage : ''} \n\n [pie.design reference](https://pie.design/foundations/colour/tokens/global#${prefix})`,
-                    detail: currentGroup === global ? getTokenDetail(variableValue, isColor) : undefined,
+					description: `Some neat description for \`${variableName}\` goes here! \n\n ${isGlobal ? globalTokenMessage : ''} \n\n [pie.design reference](https://pie.design/foundations/colour/tokens/global#${prefix})`,
+                    detail: isColor ? getColorTokenDetail(variableValue, isGlobal) : undefined,
 					kind: isColor ? vscode.CompletionItemKind.Color : vscode.CompletionItemKind.Variable,
-                    label: `${prefix.replace('dt-', '')} - PIE Design Token ${currentGroup === global ? '(Global)' : '(Alias)'}`,
+                    label: `${prefix.replace('dt-', '')} - PIE Design Token ${isGlobal ? '(Global)' : '(Alias)'}`,
 					prefix,
 				};
             }
